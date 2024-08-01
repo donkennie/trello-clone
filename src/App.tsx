cfcf9626b748a24d11e933a8a3098e93a4c31dbc
donkennie <ajeigbekehinde160@gmail.com>
2024-08-01 10:36:15 +0100

    add use focus for the input object and component for the add new item

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import { AppContainer } from "./styles";
 import {Column} from "./Column"
+import { AddNewItem } from "./AddNewItem";
 
 export const App = () => {
   return (
     <AppContainer>
       <Column text ="Todo:"></Column>
+      {/* <Column text ="In Progress:"></Column> */}
+      <AddNewItem
+         toggleButtonText = "+ Add another list"
+         onAdd={console.log}
+      />
     </AppContainer>
   )
 }
