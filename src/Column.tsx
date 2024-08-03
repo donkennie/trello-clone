e95de6d36db07a639ddceebefb2f361f26b901ca
donkennie <ajeigbekehinde160@gmail.com>
2024-08-03 21:41:34 +0100

    refactor the components

--- src/Column.tsx
+++ src/Column.tsx
@@ -1,18 +1,20 @@
 import {ColumnContainer, ColumnTitle} from "./styles"
 import {Card} from "./Card"
 import { AddNewItem } from "./AddNewItem"
+import { useAppState } from "./state/AppStateContext"
 
 type ColumnProps = {
     text: string
+    id: string
 }
 
 export const Column = ({text}: ColumnProps) =>{
     return (
         <ColumnContainer>
             <ColumnTitle>{text}</ColumnTitle>
-            <Card text="Generate app scaffold"></Card>
-            <Card text="Learn TypeScript"></Card>
-            <Card text="Begin to use static typing"></Card>
+            {tasks.map((task) =>(
+                <Card text= {task.text} key={task.id} id={task.id} />
+            ))}
             <AddNewItem
                 toggleButtonText="+ Add another card"
                 onAdd={console.log}
